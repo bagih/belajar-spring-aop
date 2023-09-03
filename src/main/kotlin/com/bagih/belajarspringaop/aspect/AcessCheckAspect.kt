6651d4fa53e486e80ba2c175b2bed5d55ee307dd
Bagi Hartawan <hartawanbagie@gmail.com>
2023-09-03 21:57:38 +0800

    Pointcut Expressions

--- src/main/kotlin/com/bagih/belajarspringaop/aspect/AcessCheckAspect.kt
+++ src/main/kotlin/com/bagih/belajarspringaop/aspect/AcessCheckAspect.kt
@@ -2,6 +2,7 @@ package com.bagih.belajarspringaop.aspect
 
 import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration
 class AccessCheckAspect {
     val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
+    @Before("execution(* com.bagih.belajarspringaop..*.*Filtering()) || execution(String com.bagih.belajarspringaop..*.*(..))")
     fun before(joinPoint: JoinPoint){
         logger.info("intercepted call before execution: {}", joinPoint)
     }
